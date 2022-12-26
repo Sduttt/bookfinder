cdca63d653aefd4178366193e6c0d7d4a4ae362d
Sduttt <subhamdutta588@gmail.com>
2022-12-26 20:13:07 +0530

    result card text color changed

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,7 @@ function Result({img, book_title, author, year, page}) {
       <figure>
         <img src={img} alt="Cover" className="w-40 h-56" />
       </figure>
-      <div className="card-body my-[-5px] mx-[-20px] w-72">
+      <div className="card-body my-[-5px] mx-[-20px] w-72 text-white">
         <h2 className="card-title my-[-5px]">{book_title} </h2>
         <h3>Writer: <span className="italic text-clip w-64 h-44">{author}</span> </h3>
         <p className="">First Publishing year: <span className="italic"> {year} </span> </p>
